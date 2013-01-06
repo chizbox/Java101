3a4d2b90620cf73b6805ac079a9593a98c6af978
Paul <Paul@192.168.254.102>
2013-01-06 10:32:31 +0800

    a

---
+++ Java101/src/com/java101/bucky/oop/tuna.java
@@ -0,0 +1,15 @@
+package com.java101.bucky.oop;
+
+public class tuna extends food{
+	
+	//overrides eat method from food
+	public void eat(){
+		System.out.println("This tuna is great");
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
